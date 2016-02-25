b57d2dbe2b821f545fd525624a29c77714c74222
LinkStrifer <linkstrifer@outlook.com>
2016-02-25 00:26:58 -0500

    [add] blacklist

--- elements/schedule-match.js
+++ elements/schedule-match.js
@@ -19,6 +19,8 @@ Polymer({
         var matches = data.tournament.matches;
         var participants = [];
         var scores = [];
+        var blacklist = [34218850, 34218844];
+        
         function addPoints(element, index, array) {
             if(element.match.state === 'complete') {
                 var score = element.match.scores_csv.split('-');
@@ -28,27 +30,49 @@ Polymer({
                 scores[ID2] = (scores[ID2] === undefined) ? Number(score[1]) : scores[ID2] + Number(score[1]);
             }
         }
+        
         function sortPositions(a, b) {
             return a.score > b.score ? -1 : 1;
         }
+
+        function isBlacklist(id) {
+            if(blacklist.indexOf(id) === -1) {
+                return false;
+            }
+            
+            return true;
+        }
+        
         matches.find(addPoints);
+        
         for(var i = 0; i < data.tournament.participants.length; i++) {
             var tempParticipant = data.tournament.participants[i].participant;
+            
             scores[tempParticipant.id] = (scores[tempParticipant.id] === undefined) ? 0 : scores[tempParticipant.id];
+
             participants.push({
                 'id': tempParticipant.id,
                 'name': tempParticipant.display_name,
                 'score': scores[tempParticipant.id]
             });
         }
+
         participants.sort(sortPositions);
-        for(var i = 0; i < data.tournament.participants.length; i++) {
+        
+        for(var i = 0; i < participants.length; i++) {
+            if(isBlacklist(participants[i].id)) {
+                participants.splice(i, 1);
+            }
+        }
+
+        for(var i = 0; i < participants.length; i++) {
             if(i + 1 < 10) {
                 participants[i].rank = '#0' + (i + 1);
             } else {
                 participants[i].rank = '#' + (i + 1);
             }
         }
+
         self.participants = participants;
     },
     ready: function() {
